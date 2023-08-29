3eb7a16a4564e8ebac426dbded4474e2d876c7f6
solstice23 <solstice233@gmail.com>
2023-08-29 03:05:34 -0400

    Remove unnecessary code

--- src/components/Tab.jsx
+++ src/components/Tab.jsx
@@ -1,24 +1,3 @@
-import classNames from 'classnames';
-import { forwardRef, useEffect, useRef } from 'react';
-import { createPortal } from 'react-dom';
-import css from './Tabs.module.scss';
-import useScopedThemeClass from '../hooks/useScopedThemeClass.js';
-
-const Tab = forwardRef(function Tab(props, ref) {
-	if (ref === null) ref = useRef();
-
-	return (
-		<div className={
-			classNames(
-				css.tab,
-				props.className,
-			)
-		}
-		ref={ref}>
-			test
-		</div>
-	)
-});
-
+const Tab = () => null;
 
 export default Tab;
